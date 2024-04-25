26b8fda4fc718f45a301dde9683813e0a1fa1064
𝐍ᴀᴋs𝐇  𝐕ᴀɪ <166161356+Wizardtexh@users.noreply.github.com>
2024-04-25 21:19:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 # VALUES REQUIRED FOR XBOTS
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 29109422
+API_HASH = "36ec891e0a5cdf8c1af3c20772dbbd6a"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
@@ -27,10 +27,10 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5518687442").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="7080052423").split()))
 for x in OXYGEN:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="5059737154"))
+OWNER_ID = int(getenv("OWNER_ID", default="7080052423"))
 SUDO_USERS.append(OWNER_ID)
 
 
