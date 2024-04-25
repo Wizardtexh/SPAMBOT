4cd3728c27c71de915f7f90acfa307a553631856
𝐍ᴀᴋs𝐇  𝐕ᴀɪ <166161356+Wizardtexh@users.noreply.github.com>
2024-04-25 21:23:46 +0530

    Update start.py

--- OXYBOT/modules/start.py
+++ OXYBOT/modules/start.py
@@ -8,11 +8,11 @@ START_BUTTON = [
         Button.inline("★𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦★", data="help_back")
     ],
     [
-        Button.url("★𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥★", "https://t.me/OXEGN"),
-        Button.url("★𝗦𝗨𝗣𝗣𝗢𝗥𝗧★", "https://t.me/+XpL0qhdF7TQzNDVl")
+        Button.url("★𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥★", "https://t.me/btwnaksh"),
+        Button.url("★𝗦𝗨𝗣𝗣𝗢𝗥𝗧★", "PAPANAKSH")
     ],
     [
-        Button.url("★𝗖𝗢𝗗𝗘𝗥★", "https://t.me/PRADHAN474")
+        Button.url("★𝗖𝗢𝗗𝗘𝗥★", "https://t.me/btwnaksh")
     ]
 ]
 
@@ -35,7 +35,7 @@ async def start(event):
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})**\n"
         await event.client.send_file(
             event.chat_id,
-            "https://telegra.ph/file/b11e7d86e4622a3b3e54e.jpg",
+            "https://telegra.ph/file/7bb2cd836e150b02e93ef.jpg",
             caption=TEXT,
             buttons=START_BUTTON
         )
